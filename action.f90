b5950224224f389189c604550fcf21e27d0271bc
jacobsc050 <jacobsc050@gmail.com>
2023-06-23 15:32:35 -0400

    test

--- action.f90
+++ action.f90
@@ -3,17 +3,19 @@ module functions
     use converter
     implicit none
 contains
-        real function action_equation(lattice, dim, size) result(return_value)
+        real function action_equation(lattice, dim, size, params) result(return_value)
         integer:: dim, size, j   
         real, dimension(size ** dim) :: lattice
-        real :: tot_action, action_at_a_point, const_1 = 1, const_2 = 1 !neeed to make consts something more realistic
+        real, dimension(2) :: params 
+        real :: tot_action, action_at_a_point !neeed to make consts something more realistic
         integer :: left, right, top, bottom
+
         write(*,*) "Calculating action:"
         if(dim==2) then 
             do j = 1,size**dim
                 call nearestNeighbors(j, left, right, top, bottom)
                 action_at_a_point = -1 * lattice(j) * (lattice(top) + lattice(bottom) + lattice(left) + lattice(right)) &
-                + const_1 * lattice(j) ** 2 + const_2 * lattice(j) ** 4
+                + params(1)* lattice(j) ** 2 + params(2) * lattice(j) ** 4
                 tot_action = tot_action + action_at_a_point 
                 return_value = tot_action
             end do 
@@ -36,11 +38,15 @@ program action
     real :: action_value
     real, dimension(:), allocatable :: flattened_lattice
     real, dimension(:), allocatable :: x,y
+    real, dimension(2) :: params
+    params(1) = 1 
+    params(2) = 2 
 
     call lattice_make(dim, size, flattened_lattice)
     call write_lattice_to_file(dim, size, flattened_lattice)
-    action_value = action_equation(flattened_lattice,dim,size)
+    action_value = action_equation(flattened_lattice,dim,size,params)
    
+
 
     write(*, "('Total action:' F100.9)") action_value
 
