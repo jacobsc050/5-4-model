bc978564dbdae141b0b83258482fd1006e4b8a45
jacobsc050 <jacobsc050@gmail.com>
2023-06-27 14:19:08 -0400

    calcsandoutput

--- action.f90
+++ action.f90
@@ -15,28 +15,42 @@ program action
     real, dimension(:), allocatable :: x,y
     real, dimension(2) :: params
     character (len=:), allocatable :: filename
-    params(1) = 1 ! mass
-    params(2) = 1 ! lambda
+    logical :: fileExists
+    integer :: unitNumber
+
+    params(1) = 1 ! kappa
+    params(2) = 1! lambda_tilda
+    
+
     filename = "pre_lattice.txt"
-   
     call lattice_make(dim, size, flattened_lattice)
     call write_lattice_to_file(dim, size, flattened_lattice, filename)
     ! action_value = action_equation(flattened_lattice,dim,size,params)
     ! probability = exp(-1 * action_value)
     ! write(*, "('Probability:' F10.27)") probability
 
-    do j = 1,1000
+    do j = 1,10000
         write(*,'(A, I0)') "j: ", j 
         flattened_lattice = metropolis_hastings(flattened_lattice, dim, size, params) 
     end do 
     filename = "post_lattice.txt"
     call write_lattice_to_file(dim, size, flattened_lattice, filename)
 
+    filename = "output_MH.txt"
+
+    ! Check if the file already exists
+    inquire(file=trim(fileName), exist=fileExists)
+
+    if (fileExists) then
+        ! Open the file in write mode if it doesn't exist
+        
+    end if
+
     do j = 1,1000
         write(*,'(A, I0)') "j: ", j 
         action_value = action_equation(flattened_lattice,dim, size, params)
         flattened_lattice = metropolis_hastings(flattened_lattice, dim, size, params) 
-        call calculateAndOutputResults(flattened_lattice,size, "output_MH.txt", action_value)
+        call calculateAndOutputResults(flattened_lattice,size, filename, action_value)
     end do 
         
     
